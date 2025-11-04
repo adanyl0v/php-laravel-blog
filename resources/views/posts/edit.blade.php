d4fe6052c7808b06b055754f7fe8fa877744bfc3
Artem Danylov <a.danylov39@gmail.com>
2025-11-04 12:34:13 +0200

    Fix bugs
    - Post page now displays its title
    
    - Show modify post button only for authors
    
    - Show tags when editing post

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -35,6 +35,27 @@
                                       class="block mt-1 w-full border-gray-300 rounded-md shadow-sm">{{ old('body', $post->body) }}</textarea>
                         </div>
 
+                        <div class="mt-4">
+                            <label for="tags" class="block font-medium text-sm text-gray-700">
+                                Tags (Hold Ctrl or Cmd to select multiple)
+                            </label>
+
+                            @php
+                                $postTagIds = $post->tags->pluck('id');
+                            @endphp
+
+                            <select id="tags" name="tags[]" multiple
+                                    class="block mt-1 w-full border-gray-300 rounded-md shadow-sm h-40">
+                                @foreach ($tags as $tag)
+                                    <option value="{{ $tag->id }}"
+                                            @if ($postTagIds->contains($tag->id)) selected @endif
+                                    >
+                                        {{ $tag->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
                         <div class="flex items-center justify-end mt-4">
                             <button type="submit"
                                     class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,16 +14,23 @@
                         <a href="{{ route('posts.index') }}" class="text-indigo-600 hover:text-indigo-900">
                             &larr; Back to the list
                         </a>
-                        <a href="{{ route('posts.edit', $post) }}"
-                           class="inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 transition ease-in-out duration-150">
-                            Modify
-                        </a>
+
+                        @can('update', $post)
+                            <a href="{{ route('posts.edit', $post) }}"
+                               class="inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 transition ease-in-out duration-150">
+                                Modify
+                            </a>
+                        @endcan
                     </div>
+
+                    <h1 class="text-3xl font-bold mb-2 text-gray-900">
+                        {{ $post->title }}
+                    </h1>
 
                     <p class="text-sm text-gray-600">
                         Author: {{ $post->user->name }} | {{ $post->created_at->format('d.m.Y H:i') }}
                     </p>
-
+                    
                     <div class="mt-6 prose max-w-none">
                         {!! nl2br(e($post->body)) !!}
                     </div>
